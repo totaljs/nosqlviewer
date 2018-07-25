cbd52f180bd770379c5349215b7c2fb25a32a712
Peter Širka <petersirka@gmail.com>
2018-07-25 23:13:51 +0200

    Added a support for binary files.

--- public/js/default.js
+++ public/js/default.js
@@ -1,5 +1,18 @@
 var REGUID = /^\d{14,}[a-z]{3}[01]{1}$/;
 
+function toString(buf) {
+	return String.fromCharCode.apply(null, new Uint16Array(buf));
+}
+
+function toBase64(buffer) {
+	var binary = '';
+	var bytes = new Uint8Array(buffer);
+	var len = bytes.byteLength;
+	for (var i = 0; i < len; i++)
+		binary += String.fromCharCode(bytes[i]);
+	return window.btoa(binary);
+}
+
 Tangular.register('nosqlrender', function(val) {
 
 	if (val === undefined)
@@ -25,6 +38,78 @@ Tangular.register('nosqlrender', function(val) {
 	return '<span class="nosql-json">{0}</span>'.format(Thelpers.encode(JSON.stringify(val)));
 });
 
+Tangular.register('filesize', function(value, decimals, type) {
+	return value ? value.filesize(decimals, type) : '...';
+});
+
 String.prototype.isUID = function() {
 	return this.length < 18 ? false : REGUID.test(this);
 };
+
+Number.prototype.filesize = function(decimals, type) {
+
+	if (typeof(decimals) === 'string') {
+		var tmp = type;
+		type = decimals;
+		decimals = tmp;
+	}
+
+	var value;
+
+	// this === bytes
+	switch (type) {
+		case 'bytes':
+			value = this;
+			break;
+		case 'KB':
+			value = this / 1024;
+			break;
+		case 'MB':
+			value = filesizehelper(this, 2);
+			break;
+		case 'GB':
+			value = filesizehelper(this, 3);
+			break;
+		case 'TB':
+			value = filesizehelper(this, 4);
+			break;
+		default:
+
+			type = 'bytes';
+			value = this;
+
+			if (value > 1023) {
+				value = value / 1024;
+				type = 'KB';
+			}
+
+			if (value > 1023) {
+				value = value / 1024;
+				type = 'MB';
+			}
+
+			if (value > 1023) {
+				value = value / 1024;
+				type = 'GB';
+			}
+
+			if (value > 1023) {
+				value = value / 1024;
+				type = 'TB';
+			}
+
+			break;
+	}
+
+	type = ' ' + type;
+	return (decimals === undefined ? value.format(2).replace('.00', '') : value.format(decimals)) + type;
+};
+
+function filesizehelper(number, count) {
+	while (count--) {
+		number = number / 1024;
+		if (number.toFixed(3) === '0.000')
+			return 0;
+	}
+	return number;
+}
