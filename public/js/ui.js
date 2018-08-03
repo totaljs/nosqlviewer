4443dc61bcc15b66fd21d0e080b390afe6db36b6
Peter Širka <petersirka@gmail.com>
2018-08-03 11:26:46 +0200

    Added `copy` to clipboard.

--- public/js/ui.js
+++ public/js/ui.js
@@ -2274,3 +2274,87 @@ COMPONENT('textbox', function(self, config) {
 		config.error && self.find('.ui-textbox-helper').tclass('ui-textbox-helper-show', invalid);
 	};
 });
+
+COMPONENT('clipboard', function(self) {
+
+	var container;
+
+	self.singleton();
+	self.readonly();
+
+	self.copy = function(value) {
+		container.val(value);
+		container.focus();
+		document.execCommand('selectAll', false, null);
+		document.execCommand('copy');
+		container.blur();
+	};
+
+	self.make = function() {
+		var id = 'clipboard' + self.id;
+		$(document.body).append('<textarea id="{0}" class="ui-clipboard"></textarea>'.format(id));
+		container = $('#' + id);
+	};
+
+	self.setter = function(value) {
+		value && self.copy(value);
+	};
+});
+
+COMPONENT('snackbar', 'timeout:3000;button:Dismiss', function(self, config) {
+
+	var show = true;
+	var callback;
+
+	self.readonly();
+	self.blind();
+	self.make = function() {
+		self.aclass('ui-snackbar hidden');
+		self.append('<div><a href="javasc' + 'ript:void(0)" class="ui-snackbar-dismiss"></a><div class="ui-snackbar-body"></div></div>');
+		self.event('click', '.ui-snackbar-dismiss', function() {
+			self.hide();
+			callback && callback();
+		});
+	};
+
+	self.hide = function() {
+		clearTimeout2(self.ID);
+		self.rclass('ui-snackbar-visible');
+		setTimeout(function() {
+			self.aclass('hidden');
+		}, 1000);
+		show = true;
+	};
+
+	self.success = function(message, button, close) {
+		self.show('<i class="fa fa-check-circle ui-snackbar-icon"></i>' + message, button, close);
+	};
+
+	self.warning = function(message, button, close) {
+		self.show('<i class="fa fa-times-circle ui-snackbar-icon"></i>' + message, button, close);
+	};
+
+	self.show = function(message, button, close) {
+
+		if (typeof(button) === 'function') {
+			close = button;
+			button = null;
+		}
+
+		callback = close;
+
+		self.find('.ui-snackbar-body').html(message);
+		self.find('.ui-snackbar-dismiss').html(button || config.button);
+
+		if (show) {
+			self.rclass('hidden');
+			setTimeout(function() {
+				self.aclass('ui-snackbar-visible');
+			}, 50);
+		}
+
+		clearTimeout2(self.ID);
+		setTimeout2(self.ID, self.hide, config.timeout + 50);
+		show = false;
+	};
+});
